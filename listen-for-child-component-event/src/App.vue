81289cc332c3c3d3d0fa777afbc4b54de03d43d3
Joe Chen <geometryolife@gmail.com>
2023-09-02 00:06:19 +0800

    add listen-for-child-component-event

---
+++ listen-for-child-component-event/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <HelloWorld
+  msg="listen-for-child-component-event"
+  @plusOneEvent="handlePlusOneEvent"
+/>
+  <div id="counter">Counter: {{ counter }}</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+  // eslint-disable-next-line
+  private counter: number = 0;
+
+  handlePlusOneEvent() {
+    console.log("handlePlusOneEvent");
+
+    // 计数器进行递增操作
+    this.counter++;
+  }
+}
+</script>
