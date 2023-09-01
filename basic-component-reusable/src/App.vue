9fc825e1327aa6b0fdbe901ed4f260b02e0e3415
Joe Chen <geometryolife@gmail.com>
2023-09-01 23:43:36 +0800

    basic-component-reusable is done

--- basic-component-reusable/src/App.vue
+++ basic-component-reusable/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="basic component"/>
+  <HelloWorld msg="basic component reuseble"/>
 </template>
 
 <script lang="ts">
@@ -14,14 +14,3 @@ import HelloWorld from './components/HelloWorld.vue';
 })
 export default class App extends Vue {}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- basic-component-reusable/src/main.ts
+++ basic-component-reusable/src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') // 应用实例被挂载到 DOM 元素 app 中
