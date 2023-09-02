26f581040e50a094a8f2a1c610fa80e6a8874872
Joe Chen <geometryolife@gmail.com>
2023-09-02 17:19:24 +0800

    add event-communication

---
+++ event-communication/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <CounterClick @plusOneEvent="handlePlusOneEvent" />
+  <CounterShow :count="counter" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import CounterClick from './components/CounterClick.vue';
+import CounterShow from './components/CounterShow.vue';
+
+@Options({
+  components: {
+    CounterClick,
+    CounterShow
+  },
+})
+export default class App extends Vue {
+  // eslint-disable-next-line
+  private counter: number = 0;
+
+  handlePlusOneEvent() {
+    console.log("handlePlusOneEvent");
+
+    // 计数器进行递增操作
+    this.counter++;
+  }
+}
+</script>
---
+++ event-communication/src/components/CounterClick.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <button v-on:click="plusOne">递增</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  emits: ["plusOneEvent"],
+})
+export default class HelloWorld extends Vue {
+  // 定义一个组件方法
+  plusOne() {
+    console.log("emit event");
+
+    // 发送自定义事件
+    this.$emit("plusOneEvent");
+  }
+}
+</script>
